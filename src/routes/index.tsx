33602cc9031750930fbec341edb49ea47f07dab3
Sophie Kwon <seohee.sophie.kwon@gmail.com>
2019-10-10 23:34:39 +0900

    Add react-route-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,31 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Lock } from 'styled-icons/material';
 import { GlobalStyle } from '../config/styles';
 import Header from '../components/Header';
+
+interface RouterProps {
+  path: string;
+  component: React.ReactNode;
+}
+
+const RouteComponent: React.FC<RouterProps> = (props) => (
+  <Route path={props.path}>
+    { props.component }
+  </Route>
+);
 
 export default () => (
   <React.Fragment>
     <GlobalStyle />
     {/* Navigation here */}
-    <Header />
+    <Router>
+      <Header />
+      <Switch>
+        <RouteComponent path='/log' component={<p>this is log</p>} />
+        <RouteComponent path='/search' component={<p>search page~~</p>} />
+      </Switch>
+
+    </Router>
   </React.Fragment>  
 );
